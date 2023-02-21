8ce98b4d1041fe2ff9f13e372e04c2990bf4fa47
SONO <sono.stream8580@gmail.com>
2023-02-21 22:33:40 +0900

    Fix evaluate function

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ class Evaluator:
             チューニングを実施
         '''
         study = optuna.create_study()
-        study.optimize(self.do_trial(), n_trials=300)
+        study.optimize(self.do_trial(), n_trials=100)
         print(study.best_params)
 
         return study.best_params
@@ -26,7 +26,7 @@ class Evaluator:
             bottom = trial.suggest_int('bottom', 0, 255)
             top = trial.suggest_int('top', 0, 255)
             tar = self.apply(bottom, top)
-            return self.evaluate(tar)
+            return self.evaluate2(tar)
 
         return objective
 
@@ -40,20 +40,37 @@ class Evaluator:
 
     def evaluate(self, target):
         '''
-            精度評価
+            精度評価。シンプルな正解率計算
         '''
         # 精度評価
-        # print(target)
-        # print(output)
-        all = target.size  # チャネル数で割ってピクセル数を取得
+        all = target.size
         score = all-np.count_nonzero(target == output)
         print(all, score)
         return score
 
+    def evaluate2(self, target):
+        '''
+            F値
+        '''
+        tp = np.count_nonzero(np.logical_and(target, output))
+        tn = np.count_nonzero(np.logical_and(
+            np.logical_not(target), np.logical_not(output)))
+        fp = np.count_nonzero(np.logical_and(target, np.logical_not(output)))
+        fn = np.count_nonzero(np.logical_and(np.logical_not(target), output))
+        print(tp, tn, fp, fn)
+        precision = tp/(tp+fp) if tp+fp > 0 else 0
+        recall = tp/(tp+fn) if tp+fn > 0 else 0
+        f_measure = 2*precision*recall / \
+            (precision+recall) if precision+recall > 0 else 0
+        # precisionかf-measureが効果的
+        return 1-precision
+
 
 if __name__ == "__main__":
-    input = cv2.imread("data_sample/grad.png")
-    output = cv2.imread("data_sample/grad_out.png")
+    # input = cv2.imread("data_sample/input.png")
+    # output = cv2.imread("data_sample/output.png")
+    input = cv2.imread("data_sample/grad2.png")
+    output = cv2.imread("data_sample/grad2_out.png")
     evaluator = Evaluator(input, output)
     best_params = evaluator.tune()
     evaluator.evaluate(input)
@@ -61,5 +78,7 @@ if __name__ == "__main__":
 
     cv2.imshow("gt", output)
     cv2.imshow("estimation", res)
-    cv2.imshow("mine", evaluator.apply(130, 140))
+    cv2.imshow("mine", evaluator.apply(119, 129))
+    # imx = evaluator.apply(119, 129)
+    # evaluator.evaluate2(imx)
     cv2.waitKey(0)
